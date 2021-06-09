43a76e8a4792f51a4ad51a86852507a86d03b0f2
RickG96 <30740904+RickG96@users.noreply.github.com>
2021-06-08 22:27:30 -0600

    prueba

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-var http = require('http');
+cargarArchivo = function() {
+    let archivos = document.getElementById("customFile").files;
+
+    let archivo = archivos[0];
+
+    let reader = new FileReader();
+
+    reader.onload = function(e) {
+        let arc = e.target.result
+        document.getElementById("taCS").value = archivoABox(arc);
+        
+    }
+    reader.readAsText(archivo)
+
+} 
+
+function archivoABox(entrada) {
+    let limpio = [];
+
+    for(let i = 0; i < entrada.length; i++) {
+        limpio.push(entrada.charAt(i));
+    }
+    
+    return limpio.join("");
+}
